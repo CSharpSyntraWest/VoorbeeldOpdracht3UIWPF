c207fa15f42b58605cd539841fd37426cbf5544a
Helena Coppieters <hcoppieters@hotmail.com>
2020-11-30 13:36:23 +0100

    Add project files.

---
+++ VoorbeeldOpdracht3UIWPF/Models/Persoon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VoorbeeldOpdracht3UIWPF.Utility;
+
+namespace VoorbeeldOpdracht3UIWPF.Models
+{
+    public class Persoon : ObservableObject
+    {
+        private string _naam;
+        public string Naam
+        {
+            get { return _naam; }
+            set { OnPropertyChanged(ref _naam, value); }
+        }
+
+        private string _telNr;
+        public string TelNr
+        {
+            get { return _telNr; }
+            set { OnPropertyChanged(ref _telNr, value); }
+        }
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set { OnPropertyChanged(ref _email, value); }
+        }
+
+    }
+
+}
---
+++ VoorbeeldOpdracht3UIWPF/ViewModels/MijnViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using VoorbeeldOpdracht3UIWPF.Models;
+using VoorbeeldOpdracht3UIWPF.Utility;
+
+namespace VoorbeeldOpdracht3UIWPF.ViewModels
+{
+    public class MijnViewModel : ObservableObject
+    {
+        private Persoon _persoon;
+        public MijnViewModel()
+        {
+            PersonenLaden();
+
+        }
+
+        private ObservableCollection<Persoon> _persoonCollectie;
+        public ObservableCollection<Persoon> PersoonCollectie
+        {
+            get { return _persoonCollectie; }
+            set
+            {
+                _persoonCollectie = value;
+                OnPropertyChanged();
+            }
+        }
+        public Persoon HuidigePersoon
+        {
+            get
+            {
+                return _persoon;
+            }
+            set
+            {
+                _persoon = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private void PersonenLaden()
+        {
+            IEnumerable<Persoon> personen = new List<Persoon>()
+            {
+                new Persoon
+                {
+                    Naam = "Jan Jansens",
+                    TelNr = "047202515",
+                    Email ="jan.jansens@personal.com",
+                },
+                new Persoon
+                {
+                    Naam = "Piet Pieters",
+                    TelNr = "0498124589",
+                    Email= "Piet.pieters@personal.com",
+                }
+            };
+
+            PersoonCollectie = new ObservableCollection<Persoon>(personen);
+
+        }
+    }
+}
